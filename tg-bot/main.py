6b9082b3cbb6354e864b6bfe19c2084bf282d3b5
Максим Ставцев <mastavtsev@edu.hse.ru>
2024-07-04 21:32:28 +0300

    Changed greeting and added keyboard

--- tg-bot/main.py
+++ tg-bot/main.py
@@ -3,18 +3,22 @@ from aiogram.enums import ParseMode  # измененный импорт
 import asyncio
 from aiogram.filters import Command  # новый импорт для фильтра команд
 
-
-API_TOKEN = '7450850829:AAH7YKXTeihzOmaiG0c-RWAN2gcSPfQAwtc'
+API_TOKEN = '7428709784:AAHybBmsbItOZJ6drhk3ALdTWAvrvBgme1c'
 NOTION_API_KEY = 'secret_pvaGGjmtgix2XU8MlKJpFoAUEjUgiR6CNW7s5av7FCI'
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
-
 @dp.message(Command(commands=['start', 'help']))  # исправленный декоратор
 async def send_welcome(message: types.Message):
-    await message.reply("Hello! I am your Notion RAG bot. Send me a query and I'll fetch the information from your Notion documents.")
+    button = types.KeyboardButton(text="Подключить Notion")
+    markup = types.ReplyKeyboardMarkup(keyboard=[[button]], resize_keyboard=True, selective=True)
 
+    greeting_message = ("Привет! Я ваш бот для Notion, который позволяет вам напрямую общаться с вашими документами. " +
+                        "Задайте мне вопрос, и я найду для вас нужную информацию.")
+
+
+    await message.answer(greeting_message, reply_markup=markup)
 
 @dp.message()
 async def handle_query(message: types.Message):
@@ -25,6 +29,7 @@ async def handle_query(message: types.Message):
 
 async def main():
     await dp.start_polling(bot)
+
 
 if __name__ == '__main__':
     asyncio.run(main())
